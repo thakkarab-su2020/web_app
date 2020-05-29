148054a40d15e53923a7a50771242c2801762cf9
Abhi Thakkar <thakkar.ab@northeastern.edu>
2020-05-29 01:42:08 -0400

    test cases added

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -73,15 +73,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+Database
+https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'MySQL',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '0072707',
         'HOST': 'localhost',
         'PORT': '3306'
     }
