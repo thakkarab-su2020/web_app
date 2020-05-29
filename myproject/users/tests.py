d17a69696e6bc2be146c79541eb3b365a22f419b
Abhi Thakkar <thakkar.ab@northeastern.edu>
2020-05-29 01:42:08 -0400

    test cases added

--- myproject/users/tests.py
+++ myproject/users/tests.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your tests here.
+
+
+
+class UnitTests(TestCase):
+    def test_register_page(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+
+    
