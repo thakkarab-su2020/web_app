11c956ce6c95f6c3f5f1c47e81db27dfb6440833
Abhi Thakkar <thakkar.ab@northeastern.edu>
2020-06-03 01:43:32 -0400

    uploading assignment 3

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django import forms
+from django.urls import reverse
+
+
+class Book(models.Model):
+	isbn = models.CharField(max_length=100)
+	title = models.CharField(max_length=100)
+	authors = models.CharField(max_length=100)
+	date_posted = models.DateTimeField(auto_now_add = True)
+	update_date = models.DateTimeField(auto_now =True , blank= True )
+	publication_date = models.DateField()
+	# publication_date= forms.DateField(label='Publication date', widget=forms.SelectDateWidget)
+	quantity = models.DecimalField(max_digits=3 , decimal_places=0)
+	price = models.DecimalField(max_digits=6 , decimal_places=2)
+	seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('book-detail', kwargs={'pk' : self.pk})
+
+
+	def get_update_cart_url(self):
+		return reverse("update-cart", kwargs={'pk': self.pk})
+	
+	def get_delete_from_cart_url(self):
+		return reverse("delete-cart", kwargs={'pk': self.pk})
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return "{} of {}".format(self.quantity, self.item.title)
+
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+    def get_final_price(self):
+        return self.get_total_item_price()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        return total
+
+
+
+
+
 
 
--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from .models import Book , OrderItem, Order
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin 
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView, View)
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.utils import timezone
+from django import template
+from django.core.exceptions import ObjectDoesNotExist
+
+
 
 posts = 'This is a basic signin signup web application'
-	
-
-
-def home(request):
-	return render(request,'ui/home.html',{'title':'Home page'})
 
 def about(request):
 	context={
@@ -14,3 +20,171 @@ def about(request):
 		
 	}
 	return render(request,'ui/about.html',context)
+	
+
+
+def home(request):
+	context ={
+		'books':Book.objects.all()
+	}
+	return render(request,'ui/home.html',context)
+
+
+class BookListView(ListView):
+	model = Book
+	template_name = 'ui/home.html'
+	context_object_name = 'books'
+	ordering = ['title','price']
+
+class BookDetailView(DetailView):
+	model = Book
+	template_name = 'ui/book_detail.html'
+
+class BookCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+	model = Book
+	fields = ['isbn','title', 'authors','publication_date','quantity','price']
+	template_name = 'ui/book_form.html'
+	success_message = "Book has been Added!"
+
+	def form_valid(self, form):
+		form.instance.seller = self.request.user
+		return super().form_valid(form)
+
+
+class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView ):
+	model = Book
+	fields = ['isbn','title', 'authors','publication_date','quantity','price']
+	template_name = 'ui/book_form.html'
+	success_message = "Book has been updated!"
+
+	def test_func(self):
+		book = self.get_object()
+		if self.request.user == book.seller:
+			return True
+
+		return False
+
+class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
+	model = Book
+	template_name = 'ui/product_confirm_delete.html'
+	success_url = '/'
+
+	def test_func(self):
+		book = self.get_object()
+		if self.request.user == book.seller:
+			return True
+		return False
+
+def update_cart(request, pk):
+	item = get_object_or_404(Book, id=pk)
+	order_item, created = OrderItem.objects.get_or_create(item = item,user= request.user,ordered=False)
+	order_qs = Order.objects.filter(user=request.user, ordered=False)
+	if order_qs.exists():
+		order = order_qs[0]
+		if order.items.filter(item__id=item.pk).exists():
+			if item.quantity == 0:
+				messages.info(request, "No more books to add")
+				return redirect("product-summary")
+			order_item.quantity +=1
+			order_item.save()
+			item.quantity -=1
+			item.save()
+			messages.info(request, "Product added to the cart")
+			return redirect("product-summary")
+		else:
+			order.items.add(order_item)
+			item.quantity -=1
+			item.save()
+			messages.info(request, "Product added to the cart")
+			return redirect ("book-detail", pk=pk)
+	
+	else:
+		ordered_date = timezone.now()
+		order = Order.objects.create(user=request.user, ordered_date = ordered_date)
+		order_items.add(order_items)
+		item.quantity -= 1
+		item.save()
+		messages.info(request, "Product has been added to the cart")
+		return redirect("product-summary")
+
+
+def delete_from_cart(request, pk):
+	item = get_object_or_404(Book, pk=pk)
+	order_qs = Order.objects.filter(user=request.user , ordered=False)
+	if order_qs.exists():
+		order = order_qs[0]
+		if order.items.filter(item__id=item.pk).exists():
+			order_item = OrderItem.objects.filter(item = item, user= request.user, ordered=False)[0]
+			item.quantity += order_item.quantity
+			item.save()
+			order.items.remove(order_item)
+			order_item.delete()
+			messages.info(request, "Product removed from the cart")
+			return redirect("product-summary")
+		else:
+			messages.info(request, "Product is not in the cart")
+			return redirect("book-detail", pk=pk)
+	else:
+		messages.info(request, "No Orders")
+		return redirect("book-detail", pk=pk)
+
+def delete_single_item(request, pk):
+	item = get_object_or_404(Book, pk=pk)
+	order_qs = Order.objects.filter(user=request.user , ordered=False)
+	if order_qs.exists():
+		order= order_qs[0]
+		if order.items.filter(item__id=item.pk).exists():
+			order_item = OrderItem.objects.filter(item=item , user= request.user, ordered =False)[0]
+			if order_item.quantity > 1:
+				order_item.quantity -= 1
+				order_item.save()
+				item.quantity +=1
+				item.save()
+				messages.info(request, "Product removed from the cart")
+				return redirect("product-summary")
+
+			else:
+				order.items.remove(order_item)
+				messages.info(request, "Product removed from the cart")
+				item.quantity +=1
+				item.save()
+				return redirect("product-summary")
+		else:
+			messages.info(request, "The item is not in the cart")
+			return redirect("product-summary")
+	else:
+		messages.info(request, "The item is not in the cart")
+		return redirect("product-summary")
+
+
+
+register = template.Library()
+
+
+@register.filter
+def item_count(user):
+    if user.is_authenticated:
+        qs = Order.objects.filter(user=user, ordered=False)
+        if qs.exists():
+            return qs[0].items.count()
+    return 0
+
+
+class ProductSummaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context = {
+                'object': order
+            }
+            return render(self.request, 'ui/product_summary.html', context)
+        except ObjectDoesNotExist:
+            messages.warning(self.request, "You do not have an active order")
+            return redirect("/")
+
+
+
+
+
+
+
