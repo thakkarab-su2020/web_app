d756b675c41f629752f6f92f37dab72a53f03958
Abhi Thakkar <thakkar.ab@northeastern.edu>
2020-07-24 13:48:00 -0400

    changes for reset password

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib import messages
+from django import forms
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm
+from .forms import UserRegisterForm, UserUpdateForm, ResetPasswd
 from django.contrib.auth import update_session_auth_hash,login,authenticate
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from blog.views import home
+from django import forms
+import boto3
+import json
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+Users = get_user_model()
+from django.contrib.auth.models import User
+import uuid
 import logging
 logger = logging.getLogger(__name__)
 from statsd import StatsClient
 metric = StatsClient()
-
+# from django.core.mail import EmailMessage
 
 def register(request):
 	if request.method == 'POST':
 		form = UserRegisterForm(request.POST)
 		if form.is_valid():
-			form.save()
+			reg = form.save(commit=False)
 			username = form.cleaned_data.get('username')
 			password = form.cleaned_data.get('password1')
+			# reg.email = username
+			reg.save()
 			user = authenticate(username=username,password=password)
 			login(request,user)
 			timer = metric.timer('registration of user')
@@ -40,6 +51,9 @@ def profile(request):
 		u_form = UserUpdateForm(request.POST, instance=request.user)
 		if u_form.is_valid():
 			u_form.save()
+			# email = EmailMessage('Nothing IMP', 'Body-Hello', to=['abhi.thakkar2707@gmail.com'])
+			# email.send()
+			# print("=============   email sent   =======================")
 			timer = metric.timer('user account update')
 			timer.start()
 			messages.success(request, "Account has been updated")
@@ -79,4 +93,38 @@ def change_password(request):
 		return render(request,'users/change_password.html',{'form':form})
 
 
+
+def password_reset(request):
+    if request.method == 'POST':
+        form = ResetPasswd(data=request.POST)
+        if form.is_valid():
+            # form.save()
+            username = form.cleaned_data.get('email')
+            if User.objects.filter(username=username).exists():
+                user1= User.objects.filter(username=username)
+                token = uuid.uuid1()
+                messages.success(request, "Link to reset your password is sent to you.")
+                client = boto3.client('sns',region_name='us-east-1')
+                msg={"username": username,"token":str(token)}
+                print(msg)
+                response = client.publish(
+                    TargetArn='arn:aws:sns:us-east-1:708581696554:user-updates-pwd',
+                    Message=json.dumps(msg),
+                    MessageAttributes={
+                    'username': {
+                        'DataType': 'String',
+                        'StringValue': username
+                    },
+                    'token': {
+                        'DataType': 'String',
+                        'StringValue': str(token)
+                    }
+                })
+            else:
+                messages.success(request, "You are not registered as a user!")
+                return HttpResponseRedirect(reverse('login'))
+            return HttpResponseRedirect(reverse('login'))
+    else:
+        form = ResetPasswd()
+        return render(request, 'users/password_reset.html', {'form': form})
 
